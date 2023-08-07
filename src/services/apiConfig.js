f51210c79f4d596fb6839bb471966e91865dd4e9
Natalie Nicole <natalienicole@MacBook-Pro-3.local>
2023-08-07 16:47:02 -0400

    api config with production and local url

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -1,7 +1,18 @@
 import axios from "axios"
 
-// const baseURL = "https://plants-api-lemon.vercel.app/"
-const baseURL = "http://localhost:3000"
+const apiURLs = {
+  production: "https://plants-api-lemon.vercel.app/",
+  development: "http://localhost:3000"
+}
+
+let baseURL = ""
+
+if (window.location.hostname === "localhost") {
+  baseURL = apiURLs.development
+}
+else {
+  baseURL = apiURLs.production
+}
 
 const api = axios.create({
   baseURL: baseURL
